f13608a4d04d56a3a7378c9d3e0962ce15804c59
Tom J Nowell <contact@tomjn.com>
2019-07-15 15:13:32 +0100

    convert all space indentations in PHP to tabs

--- plugin.php
+++ plugin.php
@@ -19,58 +19,58 @@ function tomjn_block_assets(){
 	if ( !is_singular() || !comments_open() ) {
 		return;
 	}
-    wp_enqueue_script(
-    	'tomjn_gb_js',
-    	plugins_url( 'built.js', __FILE__ ),
-    	[
-    		'wp-editor',
-    		'wp-core-data',
-    		'wp-data',
-    		'wp-block-library',
-    		'wp-format-library'
-    	],
-    	filemtime( __DIR__.'/built.js'),
-    	true
-    );
-    
-    wp_enqueue_style(
-    	'tomjn_gb_css',
-        plugins_url( 'editor.css', __FILE__ ),
-        [
-        	'media',
-        	'l10n',
-        	'buttons',
-        	'wp-editor'
-        ],
-        filemtime( __DIR__.'/editor.css'),
-        'all'
-    );
+	wp_enqueue_script(
+		'tomjn_gb_js',
+		plugins_url( 'built.js', __FILE__ ),
+		[
+			'wp-editor',
+			'wp-core-data',
+			'wp-data',
+			'wp-block-library',
+			'wp-format-library'
+		],
+		filemtime( __DIR__.'/built.js'),
+		true
+	);
+	
+	wp_enqueue_style(
+		'tomjn_gb_css',
+		plugins_url( 'editor.css', __FILE__ ),
+		[
+			'media',
+			'l10n',
+			'buttons',
+			'wp-editor'
+		],
+		filemtime( __DIR__.'/editor.css'),
+		'all'
+	);
 
-    if ( !is_admin() ) {
-        global $wp_scripts;
-        // TEMPORARY: Core does not (yet) provide persistence migration from the
-        // introduction of the block editor and still calls the data plugins.
-        // We unset the existing inline scripts first.
-        // 
-        // If we don't do this, any core blocks will break on the frontend when added
-        $wp_scripts->registered['wp-data']->extra['after'] = array();
-        wp_add_inline_script(
-            'wp-data',
-            implode(
-                "\n",
-                array(
-                    '( function() {',
-                    '   var userId = ' . intval( get_current_user_ID() ) . ';',
-                    '   var storageKey = "WP_DATA_USER_" + userId;',
-                    '   wp.data',
-                    '       .use( wp.data.plugins.persistence, { storageKey: storageKey } );',
-                    '   wp.data.plugins.persistence.__unstableMigrate( { storageKey: storageKey } );',
-                    '   wp.data.use( wp.data.plugins.controls );',
-                    '} )();',
-                )
-            )
-        );
-    }
+	if ( !is_admin() ) {
+		global $wp_scripts;
+		// TEMPORARY: Core does not (yet) provide persistence migration from the
+		// introduction of the block editor and still calls the data plugins.
+		// We unset the existing inline scripts first.
+		// 
+		// If we don't do this, any core blocks will break on the frontend when added
+		$wp_scripts->registered['wp-data']->extra['after'] = array();
+		wp_add_inline_script(
+			'wp-data',
+			implode(
+				"\n",
+				array(
+					'( function() {',
+					'   var userId = ' . intval( get_current_user_ID() ) . ';',
+					'   var storageKey = "WP_DATA_USER_" + userId;',
+					'   wp.data',
+					'       .use( wp.data.plugins.persistence, { storageKey: storageKey } );',
+					'   wp.data.plugins.persistence.__unstableMigrate( { storageKey: storageKey } );',
+					'   wp.data.use( wp.data.plugins.controls );',
+					'} )();',
+				)
+			)
+		);
+	}
 }
 
 /**
@@ -83,16 +83,16 @@ function tomjn_block_assets(){
 function wp_block_editor( $content, $name ) {
 	wp_enqueue_script('tomjn_gb_js');
 	wp_enqueue_style('tomjn_gb_css');
-    ?>
-    <input name="<?php echo esc_attr( $name ); ?>" id="<?php echo esc_attr( $name ); ?>" type="hidden" />
-    <script>
-    document.addEventListener('DOMContentLoaded', function(event) {
-        tomjn_wp_editor(
-            '<?php echo esc_js( $name ); ?>',
-            '<?php echo esc_js( $name ); ?>',
-            '<?php echo esc_js( $content ); ?>'
-        );
-    });
-    </script>
-    <?php
+	?>
+	<input name="<?php echo esc_attr( $name ); ?>" id="<?php echo esc_attr( $name ); ?>" type="hidden" />
+	<script>
+	document.addEventListener('DOMContentLoaded', function(event) {
+		tomjn_wp_editor(
+			'<?php echo esc_js( $name ); ?>',
+			'<?php echo esc_js( $name ); ?>',
+			'<?php echo esc_js( $content ); ?>'
+		);
+	});
+	</script>
+	<?php
 }
