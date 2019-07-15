a3fdb712b0b06b829a250ab8b7db76e808b2815a
Tom J Nowell <contact@tomjn.com>
2019-07-15 15:47:06 +0100

    adds a slotfillprovider component

--- js/editor.js
+++ js/editor.js
@@ -4,21 +4,26 @@ const {
 	WritingFlow,
 	ObserveTyping,
 } = wp.blockEditor;
-const { Popover } = wp.components;
+const {
+	Popover,
+	SlotFillProvider
+} = wp.components;
 const { serialize, parse } = wp.blocks;
 
 function Editor ( { startingContent, inputName }) {
 	const [ blocks, updateBlocks ] = React.useState( parse(startingContent) );
 	return <React.Fragment>
 		<BlockEditorProvider value={ blocks } onInput={ updateBlocks } onChange={ updateBlocks }>
-			<WritingFlow>
-				<ObserveTyping>
-					<div className="editor-styles-wrapper">
-						<BlockList />
-					</div>
-				</ObserveTyping>
-			</WritingFlow>
-			<Popover.Slot />
+			<SlotFillProvider>
+				<WritingFlow>
+					<ObserveTyping>
+						<div className="editor-styles-wrapper">
+							<BlockList />
+						</div>
+					</ObserveTyping>
+				</WritingFlow>
+				<Popover.Slot />
+			</SlotFillProvider>
 		</BlockEditorProvider>
 		<input type="hidden" name={inputName} value={serialize(blocks)}/>
 	</React.Fragment>
